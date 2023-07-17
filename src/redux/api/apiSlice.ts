40500c06286eda6c1d130c810b6b8d234ba74d6a
Mehedi Hasan <you@example.com>
2023-07-17 14:18:26 +0600

    feat: backend live deployment testing

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api/v1',
+    baseUrl: 'https://book-eater-backend.vercel.app/api/v1',
     prepareHeaders: (headers) => {
       headers.set('authorization', getFromLocalStorage('access-token')!);
       return headers;
